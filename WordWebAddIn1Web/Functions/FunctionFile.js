8c25bc79fcea30a19e396aee26bc9875f944773b
priyanka verma <priyanka2050@outlook.com>
2022-06-19 08:31:04 +0530

    Project setup

---
+++ WordWebAddIn1Web/Functions/FunctionFile.js
@@ -0,0 +1,49 @@
+﻿// The initialize function must be run each time a new page is loaded.
+(function () {
+    Office.initialize = function (reason) {
+        // If you need to initialize something you can do so here.
+    };
+
+    function loadSampleData(event) {
+        var buttonId = event.source.id;
+        console.log('testEventObject() called, buttonID: ' + buttonId);
+        event.complete();
+        // Run a batch operation against the Word object model.
+        Word.run(function (context) {
+
+            // Create a proxy object for the document body.
+            var body = context.document.body;
+
+            // Queue a commmand to clear the contents of the body.
+            //body.clear();
+            // Queue a command to insert text into the end of the Word document body.
+            body.insertText(
+                "This is a sample text inserted in the document via button click.",
+                Word.InsertLocation.end);
+
+            // Synchronize the document state by executing the queued commands, and return a promise to indicate task completion.
+            return context.sync();
+        })
+            .catch(errorHandler);
+    };
+    function HighlightSentences() {
+        Word.run(function (context) {
+            // Queue a command to get the current selection and then
+            // create a proxy range object with the results.
+            var document = context.document;
+
+            // This variable will keep the search results for the sentences big than 30 words.
+            var sentences = document.sentences;
+            for (var i = 0; i < sentences.length; i++) {
+                var wordsInSentence = sentences[i].Words;
+                if (wordsInSentence.Count >= 30) {
+                    sentences[i].font.highlightColor = '#FFFF00'; // Yellow
+                }                  
+            }
+            return context.sync()
+                .then(context.sync)
+                
+        })
+            .catch(errorHandler);
+    }
+})();
---
+++ WordWebAddIn1Web/Scripts/Helpers.js
@@ -0,0 +1,143 @@
+﻿/* 
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+(function () {
+    "use strict";
+    class SelectedPattern {
+        constructor(find, replace) {
+            this.find = find;
+            this.replace = replace;
+        }
+    }
+    // Create a namespace to hold application-wide settings with primitive data types.
+    var WordProcessorApp = window.WordProcessorApp || {  };
+
+    WordProcessorApp.checkedItems = ['longSentence'];
+
+
+    WordProcessorApp.CurrentSelectedPattern ;
+    
+
+    // Ensures that all table data fields are selected in the UI. 
+    WordProcessorApp.reinitializeUI = function () {
+        
+        $.each(WordProcessorApp.CheckBoxElements, function () {
+            this.check();  // Use the check method on the Office UI Fabric Checkbox component to check the checkbox.
+        });
+    }      
+
+    //Serach for a pattern and replace with other Pattern.
+    WordProcessorApp.SetResetCurrentPattern = function (event) {
+        console.log("inside set pattern");
+        var secondaryElement = $(this).parent('.ms-ListItem').children('.ms-ListItem-secondaryText');
+        var tertiaryElement = $(this).parent('.ms-ListItem').children('.ms-ListItem-tertiaryText');
+        var isChecked = !$(this).parent('.ms-ListItem').hasClass('is-selected');
+        if (isChecked) {
+            WordProcessorApp.CurrentSelectedPattern =
+                new SelectedPattern(secondaryElement[0].textContent, tertiaryElement[0].textContent);                           
+        }
+        else {
+            WordProcessorApp.CurrentSelectedPattern = new SelectedPattern("", "");
+        }
+        
+        console.log(WordProcessorApp.CurrentSelectedPattern.find);          
+    }
+    WordProcessorApp.SearchandReplace = function (event) {
+        console.log("Inside Search and replace");
+        console.log(WordProcessorApp.CurrentSelectedPattern.replace);
+    }
+    // Navigates to a different DIV when users click on the tab bar.
+    WordProcessorApp.newPage = function () {
+        if (this.id === 'HighlightTab') {
+            $('.HighlightSettings').show().siblings().hide();
+        }
+        else if (this.id === 'FindReplaceTab') {
+            $('.FindReplaceSettings').show().siblings().hide();
+        }
+        else {
+            $('.WordOveruseSettings').show().siblings().hide();
+        }
+    };
+
+        // Gets sales data, and displays the data in a table and chart.
+    WordProcessorApp.populateRegexList = function () {        
+
+        $.ajax({
+            url: 'FindReplaceRegex.json',
+            dataType: "json"
+        })
+            // Process returned data.
+        .then(function (data) {
+
+            var ul = $('#ms-List-Regex');
+            data.Patterns.forEach(function (pattern) {
+
+                var iconElement = document.createElement('i');
+                iconElement.classList.add("ms-font-xxl", "ms-fontWeight-light", "ms-fontColor-themePrimary",
+                    "ms-Icon", "ms-Icon--Play");
+                var divElement = document.createElement('div');
+                divElement.className = 'ms-ListItem-action';
+                divElement.appendChild(iconElement);
+
+                var divElement3 = document.createElement('div');
+                divElement3.className = 'ms-ListItem-actions';
+
+                divElement3.appendChild(divElement);
+
+                var divElement2 = document.createElement('div');
+                divElement2.classList.add("ms-ListItem-selectionTarget", "js-toggleSelection");
+
+                var spanPrimary = document.createElement('span');
+                spanPrimary.className = 'ms-ListItem-primaryText';
+                spanPrimary.textContent = pattern.Description;
+
+                var spanSecondary = document.createElement('span');
+                spanSecondary.className = 'ms-ListItem-secondaryText';
+                spanSecondary.textContent = pattern.Find;
+
+                var spanTertiary = document.createElement('span');
+                spanTertiary.className = 'ms-ListItem-tertiaryText';
+                spanTertiary.textContent = pattern.Replace;
+
+                var li = document.createElement('li');
+                li.classList.add("ms-ListItem", "is-selectable");
+
+                li.appendChild(spanPrimary);
+                li.appendChild(spanSecondary);
+                li.appendChild(spanTertiary);
+                li.appendChild(divElement2);
+                li.appendChild(divElement3);
+
+                ul[0].appendChild(li);
+
+            });
+        });
+    }
+
+    WordProcessorApp.setHighlightFilters = function (event) {
+
+        // Get ID of column checkbox that was changed.
+        var columnName = event.target.id;
+        if (WordProcessorApp.checkedItems.length > 0) {
+            if (WordProcessorApp.checkedItems.indexOf(columnName) !== -1) {
+                WordProcessorApp.checkedItems.push(columnName);
+            }
+            else {
+                WordProcessorApp.checkedItems.pop(columnName);
+            }
+        }
+        
+        
+    }
+   
+    WordProcessorApp.errorHandler = function (error) {
+        console.log("Error: " + error);
+        if (error instanceof OfficeExtension.Error) {
+            console.log("Debug info: " + JSON.stringify(error.debugInfo));
+        }
+    }
+
+    window.WordProcessorApp = WordProcessorApp;
+})();
